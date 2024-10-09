00400da42871eb274e84be01035873a34d410b73
CWD <cwd@lazybaer.com>
2024-10-08 19:20:52 -0500

    got pinata working

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
-import { ChatOpenAI } from '@langchain/openai';
-import { HumanMessage, SystemMessage } from '@langchain/core/messages'
-import {  JsonOutputParser, StringOutputParser } from '@langchain/core/output_parsers'
-import { DallEAPIWrapper } from "@langchain/openai"
-
 import { env } from 'process'
 
+import { ChatOpenAI } from '@langchain/openai'
+import { HumanMessage, SystemMessage } from '@langchain/core/messages'
+import { JsonOutputParser, StringOutputParser } from '@langchain/core/output_parsers'
+import { DallEAPIWrapper } from "@langchain/openai"
 
-const LLM_OAI_KEY = env.LLM_OAI_KEY ?? ''
-const MODEL_OAI = 'gpt-4o-mini'
-const STORY_PARTS = 5
-const IMAGE_STYLE = 'Arthur Rackham'
+import { AuthTestResponse, GroupResponseItem, PinataSDK, UploadResponse } from "pinata"
 
 interface StoryPart {
   title: string
   content: string
 }
 interface StoryObject {
+  title: string
   story: StoryPart[]
 }
+
+const LLM_OAI_KEY = env.LLM_OAI_KEY ?? ''
+const PINTATA_JWT = env.PINTATA_JWT ?? ''
+const PINTATA_GATEWAY = env.PINTATA_GATEWAY ?? 'aquamarine-smart-butterfly-519.mypinata.cloud'
+const MODEL_OAI = env.MODEL_OAI ?? 'gpt-4o-mini'
+const MODEL_DALLE = env.MODEL_DALLE ?? 'dall-e-3'
+const STORY_PARTS = env.STORY_PARTS ?? 3
+const IMAGE_STYLE = env.IMAGE_STYLE ?? 'Arthur Rackham'
 
 // set up connection to Open AI with langchain
 const model = new ChatOpenAI({
@@ -25,7 +30,6 @@ const model = new ChatOpenAI({
   apiKey: LLM_OAI_KEY,
   temperature: 0.75
 });
-
 
 
 // get prompt from user for story topic and style of imagery and story
@@ -36,16 +40,65 @@ const model = new ChatOpenAI({
 // push content to IPFS
 // share URL to user
 
+const pushToPinata = async (content: StoryObject, images: string[]) => {
+  const pinata = new PinataSDK({
+    pinataJwt: PINTATA_JWT,
+    pinataGateway: PINTATA_GATEWAY,
+  })
+  
+  const resp: AuthTestResponse = await pinata.testAuthentication()
+  console.log(resp)
+  if (!resp.message) {
+    console.log('bad credentials')
+    return 
+  }
+
+
+  const group: GroupResponseItem = await pinata.groups.create({
+    name: content.title,
+    isPublic: true
+  })
+
+  const imagesUploaded: UploadResponse[] = await Promise.all(images.map(async (image: string, index: number) => {
+    const upload: UploadResponse = await pinata.upload
+      .url(image)
+      .group(group.id)
+      .addMetadata({
+        name: `image_${index}`
+      })
+
+    return upload
+  }))
+
+  const contentUpload: UploadResponse[] = await Promise.all(content.story.map(async (storyPart: StoryPart, index: number) => {
+    const blob = new Blob([storyPart.content], { type: 'text/plain' })
+    const file = new File([blob], `text_${index}`, { type: 'text/plain' })
+    const upload = await pinata.upload
+      .file(file)
+      .group(group.id)
+      .addMetadata({
+        name: `text_${index}`
+    })
+    return upload
+  }))
+    
+  return { group, contentUpload, imagesUploaded }
+}
+
 const generateImage = async (prompt: string) => {
   const tool = new DallEAPIWrapper({
     n: 1, // Default
-    model: "dall-e-3", // Default
-    apiKey: process.env.OPENAI_API_KEY, // Default
+    model: MODEL_DALLE, // Default
+    apiKey: LLM_OAI_KEY
   })
 
-  const imageURL = await tool.invoke("a painting of a cat")
+  const imageURL = await tool.invoke(prompt)
+  return imageURL
+}
 
-  console.log(imageURL)
+const generateHTML = (content: string[], images: string[]) => {
+  const messages = [
+  ]
 }
 
 const generateImagePrompt = async (content: string) => {
@@ -63,14 +116,15 @@ const generateImagePrompt = async (content: string) => {
 const main = async (prompt: string) => {
   const messages = [
     new SystemMessage('You are an expert story teller, especially in regards to Grimms fairy tales and other classical stories.'),
-    new HumanMessage(`tell me a story around this concept ${prompt}. The story should have ${STORY_PARTS} parts. Return the story as a JSON object that has an array of the parts in the form "title" and "content". Only return the JSON and nothing else`),
+    new HumanMessage(`tell me a story around this concept ${prompt}. The story should have ${STORY_PARTS} parts. Return the story as a JSON object that has the title of the story in a field called "title" and an array called "story" with the story parts in the form "title" and "content". Only return the JSON and nothing else`),
   ]
 
   const resp = await model.invoke(messages)
   const parser = new JsonOutputParser()
-  const storyObj: StoryObject = { story: [] }
+  const storyObj: StoryObject = { title: 'tbd', story: [] }
   const parsed = await parser.invoke(resp)
   console.log(parsed)
+  storyObj.title = parsed.title
   storyObj.story = parsed.story
 
   const aImagePrompts = await Promise.all(storyObj.story.map(async (storyPart) => {
@@ -78,6 +132,16 @@ const main = async (prompt: string) => {
   }))
 
   console.log(aImagePrompts)
+
+  const imageURLs = await Promise.all(aImagePrompts.map(async (imagePrompt) => {
+    return await generateImage(prompt)
+  }))
+
+  console.log(imageURLs)
+
+  //save images to IPFS
+  const pinataUploads = await pushToPinata(storyObj, imageURLs)
+  console.log(pinataUploads)
 }
 
 main('a story about an anthropomorphic bear who discovers a mine full of honey')
